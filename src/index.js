d5ec468214f7e47dd369ba27645d04d4eb3d747b
Oliver <oly.rbsc@gmail.com>
2017-07-02 11:02:05 +1000

    Template upload

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// Import React
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// Import CSS
+import 'normalize.css/normalize.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap-theme.css'
+import './index.css'
+
+// Import Components
+
+
+// Index
+const Index = () => (
+	<div className="index">
+
+		<p>Test</p>
+
+	</div>
+)
+
+// Render to DOM
+ReactDOM.render(
+  <Index />,
+  document.getElementById('root')
+)
